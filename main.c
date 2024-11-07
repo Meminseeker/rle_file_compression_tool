8ebadf6c928060051eafb75d7054e2da2fa29262
Memin Seeker <muhammed.arayici@boun.edu.tr>
2024-11-07 14:29:22 +0300

    feat: use multi file functions with mock content

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include "compress_file.h"
 
 int main()
 {
     printf("Hello, World number %d!\n", 5);
+    printf("The character from compressFile is: %c\n", compressFile());
 
     return 0;
 }
