71397a8195aa6f9dd9277f5ad09ba23ef70c5705
Memin Seeker <muhammed.arayici@boun.edu.tr>
2024-11-07 15:49:29 +0300

    feat: enhance main.c to support command-line arguments for compression and decompression operations and input and output file names

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 #include "compress.h"
 #include "decompress.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    compressFile("input.txt", "compressed.txt");
-    decompressFile("compressed.txt", "output.txt");
+    if (argc < 4)
+    {
+        printf("Usage: %s <compress|decompress> <input file> <output file>\n", argv[0]);
+        return 1;
+    }
+
+    if (strcmp(argv[1], "compress") == 0)
+    {
+        compressFile(argv[2], argv[3]);
+    }
+    else if (strcmp(argv[1], "decompress") == 0)
+    {
+        decompressFile(argv[2], argv[3]);
+    }
+    else
+    {
+        printf("Invalid command. Use 'compress' or 'decompress'.\n");
+        return 1;
+    }
 
     return 0;
 }
